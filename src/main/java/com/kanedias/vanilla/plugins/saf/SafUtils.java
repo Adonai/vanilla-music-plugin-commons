19c9762d12ad7becc8d57e4de5544521a9972d96
Oleg Chernovskiy <adonai@xaker.ru>
2017-06-12 20:02:01 +0300

    Add findDocument routine for SafUtils

--- src/main/java/com/kanedias/vanilla/plugins/saf/SafUtils.java
+++ src/main/java/com/kanedias/vanilla/plugins/saf/SafUtils.java
@@ -1,8 +1,11 @@
 package com.kanedias.vanilla.plugins.saf;
 
 import android.os.Build;
+import android.support.annotation.Nullable;
+import android.support.v4.provider.DocumentFile;
 
 import java.io.File;
+import java.util.List;
 
 /**
  * Utility class for SAF-related routines
@@ -19,5 +22,34 @@ public class SafUtils {
     public static boolean isSafNeeded(File file) {
         // on external SD card after KitKat this will return false
         return !file.canWrite() && Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+    }
+
+    /**
+     * Finds needed file through Document API for SAF. It's not optimized yet - you can still gain wrong URI on
+     * files such as "/a/b/c.mp3" and "/b/a/c.mp3", but I consider it complete enough to be usable.
+     * @param currentDir - document file representing current dir of search
+     * @param remainingPathSegments - path segments that are left to find
+     * @return Document file representing a target
+     */
+    @Nullable
+    public static DocumentFile findInDocumentTree(DocumentFile currentDir, List<String> remainingPathSegments) {
+        for (DocumentFile file : currentDir.listFiles()) {
+            int index = remainingPathSegments.indexOf(file.getName());
+            if (index == -1) {
+                continue;
+            }
+
+            if (file.isDirectory()) {
+                remainingPathSegments.remove(file.getName());
+                return findInDocumentTree(file, remainingPathSegments);
+            }
+
+            if (file.isFile() && index == remainingPathSegments.size() - 1) {
+                // got to the last part
+                return file;
+            }
+        }
+
+        return null;
     }
 }
